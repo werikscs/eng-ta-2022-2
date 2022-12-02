814f47690fb11ccf5a1692b5853bba5625c952a1
Weriks Santos <werikscs@gmail.com>
2022-12-01 23:55:37 -0300

    test: failing should sum 2 vectors with 2 element each

--- src/dojo-puzzle-1.test.ts
+++ src/dojo-puzzle-1.test.ts
@@ -11,8 +11,21 @@ describe('', () => {
     const vectorB: number[] = [1]
     const sut = new VectorOperations()
     //action
+    // result = 1*1 = 1
     const result = sut.scalarProduct(vectorA, vectorB)
     //expectation
     expect(result).toBe(1)
+  })
+
+  test('should sum 2 vectors with 2 element each', () => {
+    //setup
+    const vectorA: number[] = [1,2]
+    const vectorB: number[] = [1,2]
+    const sut = new VectorOperations()
+    //action
+    // result = 1*1 + 2*2 = 5
+    const result = sut.scalarProduct(vectorA, vectorB)
+    //expectation
+    expect(result).toBe(5)
   })
 })

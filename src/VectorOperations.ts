2e3cf6a31e20cb1e3c31457ec4f650420c64fc80
Weriks Santos <werikscs@gmail.com>
2022-12-15 13:58:38 -0300

    test(p): should throw a error when vectors have different sizes

--- src/VectorOperations.ts
+++ src/VectorOperations.ts
@@ -1,5 +1,7 @@
 class VectorOperations {
   scalarProduct(vectorA: number[], vectorB: number[]): number{
+    if(vectorA.length !== vectorB.length) throw new Error('vectors should have same length')
+
     let result = 0
     
     for(let i=0; i<vectorA.length; i++)

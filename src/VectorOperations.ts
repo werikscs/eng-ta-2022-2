411498a8116242002da5e5b31bf6198347f9d390
Weriks Santos <werikscs@gmail.com>
2022-12-15 14:05:59 -0300

    test(p): should sum 2 vectors with diff sizes

--- src/VectorOperations.ts
+++ src/VectorOperations.ts
@@ -1,11 +1,9 @@
 class VectorOperations {
   scalarProduct(vectorA: number[], vectorB: number[]): number{
-    if(vectorA.length !== vectorB.length) throw new Error('vectors should have same length')
-
     let result = 0
     
     for(let i=0; i<vectorA.length; i++)
-      result += vectorA[i] * vectorB[i]
+      result += ( vectorA[i] || 0) * (vectorB[i] || 0)
 
     return result
   }
